03ed53feeb9cbab0ee733d102aa9ca815693b6fc
Martins <victorcostamartins0@gmail.com>
2025-06-10 15:10:56 -0300

    atualização de routas

--- routes/api.php
+++ routes/api.php
@@ -3,27 +3,8 @@
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\UsuarioController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
-// Route::resource('/usuario', UsuarioController::class);
-Route::get('/usuario', [UsuarioController::class, 'index']);
-Route::post('/usuario', [UsuarioController::class, 'create']);
-Route::get('/usuario/{id}', [UsuarioController::class, 'show']);
-Route::put('/usuario/{id}', [UsuarioController::class, 'update']);
-Route::delete('/usuario/{id}', [UsuarioController::class, 'destroy']);
-
-Route::get('/categoria', [CategoriaController::class, 'index']);
-Route::post('/categoria', [CategoriaController::class, 'create']);
-Route::put('/categoria/{id}', [CategoriaController::class, 'update']);
-Route::delete('/categoria/{id}', [CategoriaController::class, 'destroy']);
-
-Route::get('/book', [BookController::class, 'index']);
-Route::post('/book', [BookController::class, 'create']);
-Route::get('/book/{id}', [BookController::class, 'show']);
-Route::put('/book/{id}', [BookController::class, 'update']);
-Route::delete('/book/{id}', [BookController::class, 'destroy']);
+Route::resource('/usuario', UsuarioController::class);
+Route::resource('/categoria', CategoriaController::class);
+Route::resource('/book', BookController::class);
