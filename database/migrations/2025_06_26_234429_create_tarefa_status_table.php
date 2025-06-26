420b6f82d9ab07292e4f0d8fb27fd0d6eb27e7f9
Sr-MARTINS <victorcostamartins0@gmail.com>
2025-06-26 20:54:49 -0300

    crianção migrate tarefa_status

--- database/migrations/2025_06_26_234429_create_tarefa_status_table.php
+++ database/migrations/2025_06_26_234429_create_tarefa_status_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('tarefa_status', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 80);
             $table->timestamps();
         });
     }
