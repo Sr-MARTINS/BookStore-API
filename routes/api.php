14fd8ef967215a092fcc8c3e913f623cd1ec99c5
Sr-MARTINS <victorcostamartins0@gmail.com>
2025-06-26 19:18:20 -0300

    reajuste projeto - todo-API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\CategoriaController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
@@ -9,6 +8,4 @@ Route::post('/user', [UserController::class, 'create']);
 Route::get('/user/{id}', [UserController::class, 'show']);
 Route::put('/user/{id}', [UserController::class, 'update']);
 Route::delete('/user/{id}', [UserController::class, 'destroy']);
-
-Route::get('/categoria', [CategoriaController::class, 'index']);
 
