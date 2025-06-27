bd33abfcbff37421ff18c62341ce190832888b64
Sr-MARTINS <victorcostamartins0@gmail.com>
2025-06-27 11:44:12 -0300

    feat: cria endpoints CRUD para recurso Lista

--- app/Http/Controllers/ListaController.php
+++ app/Http/Controllers/ListaController.php
@@ -2,9 +2,97 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Lista;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ListaController extends Controller
 {
-    //
+    private $model;
+
+    public function __construct(Lista $model)
+    {
+        $this->model = $model;
+    }
+
+    public function index(): JsonResponse
+    {
+
+        $lista = $this->model->all();
+
+        if(!$lista) {
+            return response()->json([
+                'message' => 'Sem lista cadastrada'
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Listas:',
+            'data' => $lista
+        ]);
+    }
+
+    public function create(Request $request): JsonResponse
+    {
+        $dados = $request->all();
+        
+        $lista = $this->model->create($dados);
+
+        return response()->json([
+            'message' => 'Lista criada com sucesso',
+            'data' => $lista
+        ]);
+    }
+
+    public function show($id): JsonResponse
+    {
+        $lista = $this->model->find($id);
+
+        if(!$lista) {
+            return response()->json([
+                'message' => 'Essa lista nao existe ou nao foi encontrada'
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Lista:',
+            'data' => $lista
+        ]);
+    }
+
+    public function update($id, Request $request): JsonResponse
+    {
+        $lista = $this->model->find($id);
+
+        if(!$lista) {
+            return response()->json([
+                'message' => 'Lista nao encontrada'
+            ]);
+        }
+
+        $lista->update($request->all());
+
+        return response()->json([
+            'message' => 'Lista editade com sucesso',
+            'data' => $lista
+        ]);
+
+    }   
+
+    public function destroy($id): JsonResponse
+    {
+        $lista = $this->model->find($id);
+
+        if(!$lista) {
+            return response()->json([
+                'message' => 'lista nao encontrada'
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Lista deletada com sucesso',
+            'data' => $lista
+        ]);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListaController;
 
 Route::get('/user', [UserController::class, 'index']);
 Route::post('/user', [UserController::class, 'create']);
@@ -9,3 +10,8 @@ Route::get('/user/{id}', [UserController::class, 'show']);
 Route::put('/user/{id}', [UserController::class, 'update']);
 Route::delete('/user/{id}', [UserController::class, 'destroy']);
 
+Route::get('/lista', [ListaController::class, 'index']);
+Route::post('/lista', [ListaController::class, 'create']);
+Route::get('/lista/{id}', [ListaController::class, 'show']);
+Route::put('/lista/{id}', [ListaController::class, 'update']);
+Route::delete('/lista/{id}', [ListaController::class, 'destroy']);
